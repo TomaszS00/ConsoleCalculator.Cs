da897d161e723e074f3659ea65bfef2c5d6218b9
TomaszS00 <tomasz.sadlik00@gmail.com>
2021-04-29 11:39:12 +0200

    Dodaj pliki projektów.

---
+++ calculator/Classes/calculatorOperations.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator.Classes
+{
+    class calculatorOperations
+    {
+        /// <summary>
+        /// Function should add two numbers 
+        /// </summary>
+        /// <param name="first">It's first num to add</param>
+        /// <param name="second">it's second num to add</param>
+        /// <returns>Return result of two numbers (first+second)</returns>
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+
+        /// <summary>
+        /// Function should substract two numbers 
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <returns>Result of first-second</returns>
+        public int Substract(int first, int second)
+        {
+            return first - second;
+        }
+
+
+        /// <summary>
+        /// Function multiply two numbers
+        /// </summary>
+        /// <param name="first">First number to multiply</param>
+        /// <param name="second">Second number to multiply</param>
+        /// <returns>Return first*second</returns>
+        public int Multiply(int first, int second)
+        {
+            return first * second;
+        }
+
+        /// <summary>
+        /// Funtion divide two numbers
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <returns>Returns result of divide first/second</returns>
+        public int Divide(int first, int second)
+        {
+            return first / second;
+        }
+    }
+}
---
+++ calculator/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using calculator.Classes;
+
+
+
+namespace oop1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declarations of basics functions
+            var firstNum = 0;
+            var secondNum = 0;
+            var operation = ""; 
+            var sum = 0;
+            var calculator = new calculatorOperations();
+
+            Console.WriteLine("Type two number (integers) and type of mathematic operation (+ - * /)");
+
+
+            /*We don't know what user will enter into code so we must catch exception
+              in this case the most common exceptions are formatexception and overflow of memory
+              so I will catch it and inform user about it if it's needed
+            */
+            try
+            {
+                firstNum = int.Parse(Console.ReadLine());
+                secondNum = int.Parse(Console.ReadLine());
+                operation = Console.ReadLine();
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Błąd - nie podałeś prawidłowej wartości - spróbuj jeszcze raz");
+            }
+            catch (System.OverflowException)
+            {
+                Console.WriteLine("Podana liczba jest zbyt duża, podaj inną liczbę");
+            }
+
+            if (operation == "+")
+                sum = calculator.Add(firstNum, secondNum);
+            else if(operation == "-")
+                sum = calculator.Substract(firstNum, secondNum);
+            else if (operation == "*")
+                sum = calculator.Multiply(firstNum, secondNum);
+            else
+                sum = calculator.Divide(firstNum, secondNum);
+
+
+
+
+            Console.WriteLine(sum);
+
+        }
+    }
+
+
+}
